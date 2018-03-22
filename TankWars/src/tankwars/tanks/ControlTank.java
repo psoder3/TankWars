f3ca3bb5d71d2586af92c4edc3fed1ebe6edbc26
psoder3 <paulsoderquist3@gmail.com>
2018-03-22 15:13:52 -0600

    Add files via upload

---
+++ TankWars/src/tankwars/tanks/ControlTank.java
@@ -0,0 +1,20 @@
+package tankwars.tanks;
+
+import tankwars.*;
+
+/**
+ *
+ * @author psoderquist
+ */
+public class ControlTank extends Tank {
+
+    public ControlTank(String filename, Arena a) {
+        super(filename, a);
+    }
+            
+    @Override
+    public void tankAction() {
+        
+    }
+    
+}
